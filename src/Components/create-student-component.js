40ad20e31d4c5d23188694cb863e65f894be9600
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-09-23 18:52:38 +0530

    Updated

--- src/Components/create-student-component.js
+++ src/Components/create-student-component.js
@@ -2,36 +2,38 @@
 
 // Import Modules
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import StudentForm from "./StudentForm";
 
 // CreateStudent Component
 const CreateStudent = () => {
-const [formValues, setFormValues] =
-	useState({ name: '', email: '', rollno: '' })
-// onSubmit handler
-const onSubmit = studentObject => {
-	axios.post(
-'http://localhost:3000/students/create-student',
-	studentObject)
-	.then(res => {
-		if (res.status === 200)
-		alert('Student successfully created')
-		else
-		Promise.reject()
-	})
-	.catch(err => alert('Something went wrong'))
-}
-	
-// Retuern student form
-return(
-	<StudentForm initialValues={formValues}
-	onSubmit={onSubmit}
-	enableReinitialize>
-	Create Student
-	</StudentForm>
-)
-}
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    rollno: "",
+  });
+  // onSubmit handler
+  const onSubmit = (studentObject) => {
+    axios
+      .post("http://localhost:3000/create-student", studentObject)
+      .then((res) => {
+        if (res.status === 200) alert("Student successfully created");
+        else Promise.reject();
+      })
+      .catch((err) => alert("Something went wrong"));
+  };
+
+  // Retuern student form
+  return (
+    <StudentForm
+      initialValues={formValues}
+      onSubmit={onSubmit}
+      enableReinitialize
+    >
+      Create Student
+    </StudentForm>
+  );
+};
 
 // Export CreateStudent Component
-export default CreateStudent
+export default CreateStudent;

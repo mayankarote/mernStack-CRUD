5e3e42842fa2aa315a5e8d29b85d75e3978c7595
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-09-23 19:31:58 +0530

    Updated

--- src/Components/create-student-component.js
+++ src/Components/create-student-component.js
@@ -1,7 +1,7 @@
 // CreateStudent Component for add new student
 
 // Import Modules
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import StudentForm from "./StudentForm";
 
